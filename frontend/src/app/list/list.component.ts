128daf6922f0e52ee3cc85275e9a60f0a501c848
Armando <aleon45@ucmerced.edu>
2019-01-25 15:28:13 -0800

    Finished writing tests

--- frontend/src/app/list/list.component.ts
+++ frontend/src/app/list/list.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Hobby } from './../models/hobbies.model';
+import { HobbiesService } from './../services/hobbies.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  hobbies: Hobby[] = [];
+
+  sub: Subscription;
+
+  // hobbiesObs: Observable<Hobby[]>;
+
+
+  constructor(private hobbiesService: HobbiesService) { }
 
   ngOnInit() {
+
+    this.hobbiesService.getAllHobbies();
+
+    this.sub = this.hobbiesService.getHobbiesUpdatedList().subscribe((res) => {
+      this.hobbies = res;
+    });
+
+    /*this.hobbiesObs = this.hobbiesService.getHobbiesUpdatedList();
+    this.sub = this.hobbiesObs.subscribe((res) => {
+      this.hobbies = res;
+    });*/
+  }
+
+  ngOnDestroy() {
+
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
 }
