81d67f8fdda5a575f3c4ba27d89a03ed66165660
Armando <aleon45@ucmerced.edu>
2019-02-07 14:37:45 -0800

    Attempting to edit etc/nginx/conf.d/default.conf on nginx container

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
+// in production, this will make server calls based on the url of the angular project
+// we are going to reverse proxy to make sure the requests are being made to
+// the noder server application
 
+// blank will make the api calls go to http://localhost:4200/api/v1/hobbies
 export const environment = {
   production: true,
-  url: 'http://localhost:3000'
+  url: ''
 };
