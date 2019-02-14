8908c926ec82e1380acd4d85066e6d1fa89e1802
Armando <aleon45@ucmerced.edu>
2019-02-13 23:11:21 -0800

    Travis cannot encrypt multiple files. Therefore, you must encrypt a tar file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+tar xvf secrets.tar; 
 echo "$(cat deploy.pub)" >> ${HOME}/.ssh/id_rsa.pub
 echo "$(cat known_hosts)" >> ${HOME}/.ssh/known_hosts
 rm deploy.pub; 
 rm known_hosts;
+rm secrets.tar;
 ssh root@${NODE1} << 'ENDSSH';
 ls; 
 ENDSSH; 
