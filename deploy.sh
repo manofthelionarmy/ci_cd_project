2e013f5138d3a0fa08fc13369a6efe5ab0dc1a78
Armando <aleon45@ucmerced.edu>
2019-02-13 23:35:53 -0800

    Trying to fix ssh issue

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
-tar xvf secrets.tar; 
-echo "$(cat deploy.pub)" >> ${HOME}/.ssh/id_rsa.pub
+cd ${CI_CD_PROJ}
+ls
+tar xvf secrets.tar 
+echo "$(cat deploy.pub)" >> ${HOME}/.ssh/id_rsa
 echo "$(cat known_hosts)" >> ${HOME}/.ssh/known_hosts
-rm deploy.pub; 
-rm known_hosts;
-rm secrets.tar;
-ssh root@${NODE1} << 'ENDSSH';
-ls; 
-ENDSSH; 
+
+ssh-add ${HOME}/.ssh/id_rsa
+rm deploy.pub
+rm known_hosts
+rm secrets.tar
+ssh root@${NODE1} << 'ENDSSH'
+ls
+ENDSSH
