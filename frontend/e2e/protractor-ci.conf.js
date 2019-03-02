4807bb42d4f03aaa81819553cd66f76f00b1b98c
Armando <aleon45@ucmerced.edu>
2019-03-01 19:06:50 -0800

    Created a new docker-compose file to run the e2e tests. Edited travis.yml to use the correct yml file for e2e tests

--- frontend/e2e/protractor-ci.conf.js
+++ frontend/e2e/protractor-ci.conf.js
@@ -7,6 +7,9 @@ config.chromeDriver = '/usr/bin/chromedriver';
 
 config.allScriptsTimeout = 20000;
 
+// have it connect to the selenium grid
+config.baseUrl = 'http://0.0.0.0:4444/wd/hub';
+
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
