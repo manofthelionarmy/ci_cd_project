e638f0a8f1e436b31d885cb7ed72a7b1e7d03c1b
Armando <aleon45@ucmerced.edu>
2019-03-01 23:12:07 -0800

    Trying to fix protractor tests

--- frontend/e2e/protractor-ci.conf.js
+++ frontend/e2e/protractor-ci.conf.js
@@ -3,22 +3,24 @@ const config = require('./protractor.conf').config;
 // Tell protrator where the chrome driver is
 // https://gitlab.com/dasch8/angular-ci/
 // https://hub.docker.com/r/weboaks/node-karma-protractor-chrome/
-config.chromeDriver = '/usr/bin/chromedriver';
+config.chromeDriver = "/usr/bin/chromedriver";
 
 config.allScriptsTimeout = 20000;
 
-// config.directConnect = true;
+
+// use the chromeDriver, as specified
+config.directConnect = true;
 
 // have it connect to the angular app
-config.baseUrl = 'http://0.0.0.0:4200';
+config.baseUrl = "http://0.0.0.0:4200";
 
 // have it connect to selenium
-config.seleniumAddress = 'http://0.0.0.0:4444/wd/hub';
+config.seleniumAddress = "http://0.0.0.0:4444/wd/hub";
 
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox']
+    args: ['--headless', '--no-sandbox', '--disable-gpu']
   }
 };
 
