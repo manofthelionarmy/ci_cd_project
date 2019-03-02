1c0d1a5d130b189310fc2ccb1304a5cc527febfe
Armando <aleon45@ucmerced.edu>
2019-03-01 22:57:54 -0800

    Added seleniumAddress to protractor config

--- frontend/e2e/protractor-ci.conf.js
+++ frontend/e2e/protractor-ci.conf.js
@@ -7,8 +7,13 @@ config.chromeDriver = '/usr/bin/chromedriver';
 
 config.allScriptsTimeout = 20000;
 
-// have it connect to the selenium grid
-config.baseUrl = 'http://0.0.0.0:4444/wd/hub';
+// config.directConnect = true;
+
+// have it connect to the angular app
+config.baseUrl = 'http://0.0.0.0:4200';
+
+// have it connect to selenium
+config.seleniumAddress = 'http://0.0.0.0:4444/wd/hub';
 
 config.capabilities = {
   browserName: 'chrome',
