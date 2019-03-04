8a046ee6f4aeb99e49889187ea37a75205c8c003
Armando <aleon45@ucmerced.edu>
2019-03-04 15:47:31 -0800

    Added new tests to see if thhe tests will timeout in TravisCI. Passed on local machine

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -27,4 +27,34 @@ describe('workspace-project App', () => {
     expect(element(by.css('app-header nav')).getText()).toEqual('hobbies');
 
   });
+
+  it('should fill in info', () => {
+    browser.get('http://localhost:4200');
+
+    element(by.id('nameInput')).sendKeys('Armando Leon').then(() => {
+      console.log('Succesfully filled in name');
+    }).catch(() => {
+      console.log('Something went wrong while filling in name');
+    });
+
+    element(by.id('hobbyInput')).sendKeys('coding').then(() => {
+      console.log('Succesfully filled in hobby');
+    }).catch(() => {
+      console.log('Something went wrong while filling in hobby');
+    });
+
+    element(by.css('app-hobbies .btn')).click().then(() => {
+      console.log('Successfully clicked the form button');
+    });
+
+    // wait for http requests to finish
+    browser.driver.sleep(20000);
+
+    browser.wait(() => {
+      return browser.isElementPresent(element(by.cssContainingText('app-list li', 'Armando Leon likes coding')));
+    }, 30000);
+
+    expect(element(by.css('app-list li')).getText()).toEqual('Armando Leon likes coding');
+
+  });
 });
